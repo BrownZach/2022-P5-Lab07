a8b5e4e31f70527734e5f146b8a888f19588f77a
Zach <zachary.brown@vtsd.com>
2022-12-08 13:48:28 -0500

    Finish

--- Water.java
+++ Water.java
@@ -4,8 +4,8 @@ public class Water {
         g.setColor(Color.blue);
         g.fillRect(0, 640, 1000, 180);
     }
-    public static void drawWaves(Graphics g) {
-        g.setColor(new Color(0, 0, 125));
+    public static void drawWaves(Graphics g){
+        g.setColor(new Color(0, 0, 75));
         int x = -37;
         for(int waveNum = 1; waveNum <= 14; waveNum ++) {
             g.drawArc(x, 600, 75, 75, -90, 90);
